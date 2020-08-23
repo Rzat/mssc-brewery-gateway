0ddb8c23e211aeb8edadd31df25b4697fd9f42f7
rajatthakur00@gmail.com <rajat@55>
2020-08-23 17:19:03 +0530

    LocalHost Route Config for Beer Service, Closes #2

--- src/main/java/com/example/beerworks/brewerygateway/config/GoogleConfig.java
+++ src/main/java/com/example/beerworks/brewerygateway/config/GoogleConfig.java
@@ -3,13 +3,12 @@ package com.example.beerworks.brewerygateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("google")
-@Configuration
+//@Profile("google")
+//@Configuration
 public class GoogleConfig {
 
+    //just an example of routing requests  to google from localhost
     @Bean
     public RouteLocator googleRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
